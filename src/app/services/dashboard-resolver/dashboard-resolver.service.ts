ddad18ded53101ca2fc1a90d90dcfe90b86b1642
Andrew Ettenger <ettenger@gmail.com>
2017-11-07 22:18:00 -0500

    Redirect to login page if on dashboard and not logged in

--- src/app/services/dashboard-resolver/dashboard-resolver.service.ts
+++ src/app/services/dashboard-resolver/dashboard-resolver.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Router, Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 import { EnergyMetricsService } from '../energy-metrics';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class DashboardResolverService implements Resolve<any> {
 
   constructor(
-    private energyMetricsService: EnergyMetricsService
+    private router: Router,
+    private energyMetricsService: EnergyMetricsService,
+    private userService: UserService
   ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-    return Observable.forkJoin(
-      this.energyMetricsService.getSummary(),
-      this.energyMetricsService.getDailyUsage()
-    );
+    if (this.userService.isLoggedIn()) {
+      return Observable.forkJoin(
+        this.energyMetricsService.getSummary(),
+        this.energyMetricsService.getDailyUsage()
+      );
+    } else {
+      this.router.navigate(['/login']);
+      return null;
+    }
   }
 
 }
