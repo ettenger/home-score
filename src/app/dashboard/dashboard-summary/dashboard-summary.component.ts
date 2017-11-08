2b2cd25bfec3b1b06820975ff6f54110795a791f
Andrew Ettenger <ettenger@gmail.com>
2017-11-08 07:54:32 -0500

    Make gauge chart for energy score

--- src/app/dashboard/dashboard-summary/dashboard-summary.component.ts
+++ src/app/dashboard/dashboard-summary/dashboard-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { EnergyMetricsService, EnergySummary} from '../../services';
+import * as c3 from 'c3';
+import { EnergyMetricsService, EnergySummary } from '../../services';
 
 @Component({
   selector: 'app-dashboard-summary',
@@ -8,11 +9,40 @@ import { EnergyMetricsService, EnergySummary} from '../../services';
 })
 export class DashboardSummaryComponent implements OnInit {
   public energySummary: EnergySummary;
+  public scoreText: string;
 
   constructor(private energyMetricsService: EnergyMetricsService) { }
 
   ngOnInit() {
     this.energySummary = this.energyMetricsService.energySummary;
+    this.scoreText = this.energySummary.text;
+    const chart = c3.generate({
+      bindto: '#score-gauge',
+      data: {
+        columns: [
+          ['score', this.energySummary.score]
+        ],
+        type: 'gauge',
+      },
+      gauge: {
+        label: {
+          format: function(value, ratio) {
+            return value;
+          },
+          show: false
+        },
+        units: ' '
+      },
+      color: {
+        pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'],
+        threshold: {
+          values: [30, 60, 90, 100]
+        }
+      },
+      size: {
+        height: 180
+      }
+    });
   }
 
 }
